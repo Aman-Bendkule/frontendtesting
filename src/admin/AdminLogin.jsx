5b4b21377f8bce0128bbbb775ed6d6c60bdeaa08
Aman Bendkule <140185913+Aman-Bendkule@users.noreply.github.com>
2025-01-11 14:45:26 +0530

    Update AdminLogin.jsx

--- src/admin/AdminLogin.jsx
+++ src/admin/AdminLogin.jsx
@@ -12,7 +12,7 @@ const AdminLogin = () => {
 
     const handleLogin = async () => {
         try {
-            const response = await fetch('http://localhost:3000/admin-login', {
+            const response = await fetch('https://frontend-testing-alpha.vercel.app/admin-login', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ username, password }),
